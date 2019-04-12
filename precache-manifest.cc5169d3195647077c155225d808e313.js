2f41fcc6b76b1f847ef2a83789eeb2267ecd3386
sog <mohammed.hammed@parlicentre.org>
2019-04-12 08:24:10 +0100

    static files

---
+++ precache-manifest.cc5169d3195647077c155225d808e313.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c89b56dbf1811e077031118d43784c34",
+    "url": "/static/media/ticketLightMode.c89b56db.jpg"
+  },
+  {
+    "revision": "b89a8faa604f009d6778d171e9e75778",
+    "url": "/static/media/ticketDarkMode.b89a8faa.jpg"
+  },
+  {
+    "revision": "db84c831161885bf9829702651edebfc",
+    "url": "/static/media/reactnative.db84c831.png"
+  },
+  {
+    "revision": "44a63bcf38ef15e13bdb5aaff81f4972",
+    "url": "/static/media/nightlights.44a63bcf.jpg"
+  },
+  {
+    "revision": "36fa85078bb61be1b5b54f9bad0ddf5d",
+    "url": "/static/media/bg.36fa8507.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "85d838245b22ab2df1ea",
+    "url": "/static/js/main.5893e568.chunk.js"
+  },
+  {
+    "revision": "a46ace13d30a3e03d940",
+    "url": "/static/js/2.c42574b7.chunk.js"
+  },
+  {
+    "revision": "60a7c0bee2d5cabc9a9b9fe1de098f1b",
+    "url": "/index.html"
+  }
+];
