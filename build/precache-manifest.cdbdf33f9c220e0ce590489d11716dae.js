cfcf5b89d3c5077b051e177c537bc56014d9244e
sog <devhammed@gmail.com>
2020-03-19 18:05:01 +0000

    v2 portfolio files

---
+++ build/precache-manifest.cdbdf33f9c220e0ce590489d11716dae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c89b56dbf1811e077031118d43784c34",
+    "url": "/static/media/ticketLightMode.c89b56db.jpg"
+  },
+  {
+    "revision": "b89a8faa604f009d6778d171e9e75778",
+    "url": "/static/media/ticketDarkMode.b89a8faa.jpg"
+  },
+  {
+    "revision": "db84c831161885bf9829702651edebfc",
+    "url": "/static/media/reactnative.db84c831.png"
+  },
+  {
+    "revision": "36fa85078bb61be1b5b54f9bad0ddf5d",
+    "url": "/static/media/bg.36fa8507.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7b100b235f50c525e52a",
+    "url": "/static/js/main.4afb9197.chunk.js"
+  },
+  {
+    "revision": "45a00b9c326088d2a9a0",
+    "url": "/static/js/2.f6cec839.chunk.js"
+  },
+  {
+    "revision": "8980b705fa69084a8beeaa3bb42200b0",
+    "url": "/index.html"
+  }
+];
