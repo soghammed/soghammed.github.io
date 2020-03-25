2ca524d80b9879e547d7d24f9ae641b17bc82912
sog <devhammed@gmail.com>
2020-03-25 12:34:43 +0000

    Updates

--- v1/src/components/LandingPage/index.js
+++
@@ -1,5 +0,0 @@
-import About from './About/About';
-import ContactUs from './ContactUs/ContactUs';
-import LandingScreen from './LandingScreen/LandingScreen';
-import Projects from './Projects/Projects';
-import Styles from './Styles/Styles';
