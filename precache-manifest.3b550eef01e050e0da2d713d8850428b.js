5532078a22c04b851ee2c96272a596762ab41abe
soghammed <devhammed@gmail.com>
2020-09-20 00:14:44 +0100

    Updates

---
+++ precache-manifest.3b550eef01e050e0da2d713d8850428b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58c464389eb66c263545e01eeeb5803a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac335d03667449fceca5",
+    "url": "/static/css/main.27d6b204.chunk.css"
+  },
+  {
+    "revision": "ddb52e41f19d99ef9af5",
+    "url": "/static/js/2.262e4b94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.262e4b94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac335d03667449fceca5",
+    "url": "/static/js/main.0ccc59ca.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "36fa85078bb61be1b5b54f9bad0ddf5d",
+    "url": "/static/media/bg.36fa8507.jpg"
+  }
+]);
